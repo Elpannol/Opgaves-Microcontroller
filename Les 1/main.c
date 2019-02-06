d5c3d410ec548372ead0024f7b2315ac994466a9
GIJS_ASUS\Gijs <gijs.brian.joost@hotmail.com>
2019-02-06 12:33:58 +0100

    new answers week 2
    Added the new answers

--- Les 1/main.c
+++ Les 1/main.c
@@ -9,11 +9,77 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct{
 	unsigned char light;
 	unsigned int delay;
 } pattern;
+
+
+void s1(void);
+void s2(void);
+void s3(void);
+void end(void);
+void start(void);
+
+/* Define fsm (states,events) */
+typedef enum { D7, D6, D5 } ENUM_EVENTS;
+typedef enum { START, STATE_1, STATE_2, STATE_3, END } ENUM_STATES;
+typedef enum { Nott, S1, S2, S3, Start, End} ENUM_FUNC;
+
+/* Define fsm transition */
+typedef struct {
+	ENUM_FUNC finit;
+	ENUM_FUNC fbody;
+	ENUM_FUNC fexit;
+	ENUM_STATES nextState;
+} STATE_TRANSITION_STRUCT;
+
+// Sample fsm definition:
+//
+// 		| EV_D7    EV_D6		EV_D5
+// ---------------------------------
+// START| START     S1          S2
+// S1   | START     S1        	S2
+// S2   | START     S1          S3
+// S3   | START     END         END
+// END  | START     END         END
+
+STATE_TRANSITION_STRUCT fsm[5][3] = {
+	{ {S1,   S2,  Nott, START}, 	{Nott, S1, Nott, STATE_1},		{Start, Nott, Nott, STATE_2} },
+	{ {Nott, Start, Nott, START},	{S3, Nott, S2, STATE_1},	{Nott, S2, Nott, STATE_2} },
+	{ {Nott, Start, Nott, START}, 	{Nott, S1, Nott, STATE_1}, 		{Nott, S3, Nott, START} },
+	{ {Nott, Nott,  Nott, START},	{Nott, End, Nott, END}, 		{Nott, End, Nott, END} },
+	{ {Nott, Start, Nott, START},	{Nott, End, Nott, END}, 		{Nott, End, Nott, END} }
+};
+
+// State holder
+ENUM_STATES state = START;
+
+
+void runFunction(ENUM_FUNC function){
+	switch(function){
+		case S1:
+		s1();
+		break;
+		case S2:
+		s2();
+		break;
+		case S3:
+		s3();
+		break;
+		case Start:
+		start();
+		break;
+		case End:
+		end();
+		break;
+		case Nott:
+		break;
+	}
+}
 
 pattern patternStruct[] = {
  {0x00, 100}, {0x01, 100}, {0x02, 100}, {0x04, 100}, {0x10, 100}, {0x20, 100}, {0x40, 100}, {0x80, 100},
@@ -26,10 +92,89 @@ pattern patternStruct[] = {
  {0x00, 0x00}
 };
 
+
+void handleEvent(ENUM_EVENTS event)
+/* 
+short:			Change state in fsm. State change depens on event
+inputs:			event, this determines the next state in the fsm
+outputs:	
+notes:			Needs extensive testing. !! Please review execution 
+                order !!
+Version :    	DMK, Initial code
+*******************************************************************/
+{
+
+	// Call EXIT function old state
+	runFunction(fsm[state][event].fexit) ;
+
+	// Set new state, -1 means 
+	state = fsm[state][event].nextState;
+
+	runFunction(fsm[state][event].finit);
+	runFunction(fsm[state][event].fbody);
+	
+}
+
 void wait(int ms)
 {
 	for(int i=0; i<ms; i++){
 		_delay_ms(1);
+	}
+}
+
+
+//
+// State S1
+//
+void s1(void){
+	PORTD = 0b00000111;
+	wait(500);
+}
+
+//
+// State S2
+//
+void s2(void){
+	PORTD = 0b00000011;
+	wait(500);
+}
+
+//
+// State S3
+//
+void s3(void){
+	PORTD = 0b00000001;
+	wait(500);
+}
+
+//
+// State Start
+//
+void start(void){
+	PORTD = 0b00001111;
+	wait(500);
+}
+
+//
+// State Stop
+//
+void end(void){
+	PORTD = 0b00000000;
+}
+
+
+void b7a(){
+	DDRD = 0b00001111;			// Pins PORTD<7:4> input, PORTD<3:0)
+	// output
+	
+	handleEvent(D7);
+	handleEvent(D5);
+	handleEvent(D5);
+	handleEvent(D5);
+
+	// wait forever
+	while (1)
+	{
 	}
 }
 
@@ -48,17 +193,16 @@ void b2(){
 
 void b3(){
 
-	DDRD = 0b01111111;
-	PORTC = 0x10;
+	DDRD = 0b11111111;
 
 	while(1)
 	{
-		if(PINC & 0x01)
+		if(PINC & 0b00000001)
 		{
 			PORTD = 0x01;
 			wait(100);
 			PORTD = 0x00;
-			wait(1000);
+			wait(100);
 		}
 		else
 		{
@@ -77,7 +221,7 @@ void b4(){
 			wait(100);
 
 			PORTD &= BIT(i);
-			if(i != 0){			
+			if(i != 0){	
 				PORTD |= BIT(i - 1);
 			}
 			else{
@@ -104,31 +248,36 @@ void b5(){
 
 void b6(){
 	DDRD = 0b11111111;
-	PORTC = 0x10;
 	int delayInt = 500;
+	int pushed = 0;
 
 	while(1)
 	{
-		int pushed = 0;
-		if(PINC & 0x01 & !pushed)
-		{
-			if(delayInt == 500)
-			delayInt = 125;
-			else
-			delayInt = 500;
-		}
-
 		PORTD = 0x10;
 		wait(delayInt);
 		PORTD = 0x00;
 		wait(delayInt);
+
+		if(PINC & 0x01)
+		{
+			if(delayInt == 500 && pushed == 0)
+				delayInt = 125;
+			else if(delayInt == 125 && pushed == 0)
+				delayInt = 500;
+				
+			pushed = 1;
+		}
+		else{
+			pushed = 0;
+		}
 	}
 }
+
 
 int main(void)
 {
     /* Replace with your application code */
-	b6();   
+	b7a();   
 	return 0;
 }
 
