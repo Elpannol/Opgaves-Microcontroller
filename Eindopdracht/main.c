c7afa53f7dafe4c13fa33399da7ce90a15b85608
GIJS_ASUS\Gijs <gijs.brian.joost@hotmail.com>
2019-03-27 11:27:55 +0100

    Deleted the need voor LCD
    No LCD anymore

--- Eindopdracht/main.c
+++ Eindopdracht/main.c
@@ -8,125 +8,14 @@
 
 #define BIT(x)	(1 << (x))
 
-#define LCD_E 	3
-#define LCD_RS	2
-
-void lcd_strobe_lcd_e(void);
-void lcd_write_data(unsigned char byte);
-void lcd_write_command(unsigned char byte);
-void clear();
-void init();
-
-char toChar[10];
-
 // wait(): busy waiting for 'ms' millisecond
 // Used library: util/delay.h
 void wait( int ms )
 {
 	for (int tms=0; tms<ms; tms++)
 	{
-		_delay_ms( 1 );			// library function (max 30 ms at 8MHz)
+		_delay_ms(1);			// library function (max 30 ms at 8MHz)
 	}
-}
-
-void lcd_strobe_lcd_e(void)
-/*
-short:			Strobe LCD module E pin --__
-inputs:
-outputs:
-notes:			According datasheet HD44780
-Version :    	DMK, Initial code
-*******************************************************************/
-{
-	PORTC |= (1<<LCD_E);	// E high
-	wait(1);			// nodig
-	PORTC &= ~(1<<LCD_E);  	// E low
-	wait(1);			// Je weet maar nooit
-}
-
-void clear()
-{
-	lcd_write_command(0x01);
-	lcd_write_command(0x02);
-}
-
-
-void init(){
-	// PORTC output mode and all low (also E and RS pin)
-	DDRC = 0xFF;
-	PORTC = 0x00;
-
-	// Step 2 (table 12)
-	PORTC = 0x20;	// function set
-	lcd_strobe_lcd_e();
-
-	// Step 3 (table 12)
-	PORTC = 0x20;   // function set
-	lcd_strobe_lcd_e();
-	PORTC = 0x80;
-	lcd_strobe_lcd_e();
-
-	// Step 4 (table 12)
-	PORTC = 0x00;   // Display on/off control
-	lcd_strobe_lcd_e();
-	PORTC = 0xF0;
-	lcd_strobe_lcd_e();
-
-	// Step 5 (table 12)
-	PORTC = 0x00;   // Entry mode set
-	lcd_strobe_lcd_e();
-	PORTC = 0x60;
-	lcd_strobe_lcd_e();
-	
-	clear();
-	wait(500);
-
-}
-
-void display_text(char *str){
-	for(;*str; str++){
-		lcd_write_data(*str);
-	}
-}
-
-void lcd_write_data(unsigned char byte)
-/*
-short:			Writes 8 bits DATA to lcd
-inputs:			byte - written to LCD
-outputs:
-notes:			According datasheet HD44780 table 12
-Version :    	DMK, Initial code
-*******************************************************************/
-{
-	// First nibble.
-	PORTC = byte;
-	PORTC |= (1<<LCD_RS);
-	lcd_strobe_lcd_e();
-
-	// Second nibble
-	PORTC = (byte<<4);
-	PORTC |= (1<<LCD_RS);
-	lcd_strobe_lcd_e();
-}
-
-void lcd_write_command(unsigned char byte)
-/*
-short:			Writes 8 bits COMMAND to lcd
-inputs:			byte - written to LCD
-outputs:
-notes:			According datasheet HD44780 table 12
-Version :    	DMK, Initial code
-*******************************************************************/
-{
-	// First nibble.
-	PORTC = byte;
-	PORTC &= ~(1<<LCD_RS);
-	lcd_strobe_lcd_e();
-
-	// Second nibble
-	PORTC = (byte<<4);
-	PORTC &= ~(1<<LCD_RS);
-	lcd_strobe_lcd_e();
 }
 
 void adcInit( void )
@@ -137,29 +26,21 @@ void adcInit( void )
 
 int main( void )
 {
-	DDRB = 0xFF; //putting portB and portD as output pins
-
 	DDRD = 0xFF;
 	DDRF = 0;	//Taking portF as input.
 	adcInit();
-	init();
-	ADCSRA |=(1<<ADSC);  //starting the ADC conversion
+	ADCSRA |= (1<<ADSC);  //starting the ADC conversion
 	float i = 0;
-
-	float LDR= 0; //storing digital output
-
-	char LDRSHOW [7]; //displaying digital output as resistance in 16*2 lcd
 
 	while (1)
 	{
-		i = ADC/10.0;
+		i = ADC;
 
-		dtostrf(i, 4, 1, LDRSHOW);
-		
-		wait(100);
-		display_text(LDRSHOW);
-		memset(toChar, 0, 10);
-		wait(500);				// every 100 ms (busy waiting)
-		clear();
+		if(i > 54400){
+			PORTD ^= (1<<7);	// Toggle PORTD.7
+			wait(100);
+			//Do something when it is too loud
+			//TODO make this wotk with the buzzer and figure out what the best value is for the sound recognition
+		}
 	}
 }
